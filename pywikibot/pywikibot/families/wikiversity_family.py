61792bb30f60a2ea2300a14e90b415989283cf96
ianhussey <ian.hussey@ugent.be>
2017-09-12 00:30:50 +0200

    changed dirs of pywikibot

---
+++ pywikibot/pywikibot/families/wikiversity_family.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Family module for Wikiversity."""
+#
+# (C) Pywikibot team, 2007-2017
+#
+# Distributed under the terms of the MIT license.
+#
+from __future__ import absolute_import, unicode_literals
+
+from pywikibot import family
+
+__version__ = '$Id: 2b3760d39154c5d5b90f6173ce398a2eca479a6c $'
+
+
+# The Wikimedia family that is known as Wikiversity
+class Family(family.SubdomainFamily, family.WikimediaFamily):
+
+    """Family class for Wikiversity."""
+
+    name = 'wikiversity'
+
+    def __init__(self):
+        """Constructor."""
+        self.languages_by_size = [
+            'de', 'en', 'fr', 'ru', 'cs', 'it', 'beta', 'pt', 'es', 'ar', 'sv',
+            'fi', 'sl', 'el', 'hi', 'ja', 'ko',
+        ]
+
+        super(Family, self).__init__()
+
+        self.category_redirect_templates = {
+            '_default': (),
+            'ar': ('قالب:تحويل تصنيف',),
+            'en': ('Category redirect',),
+        }
+
+        # Global bot allowed languages on
+        # https://meta.wikimedia.org/wiki/BPI#Current_implementation
+        # & https://meta.wikimedia.org/wiki/Special:WikiSets/2
+        self.cross_allowed = ['ja', 'ko', ]
